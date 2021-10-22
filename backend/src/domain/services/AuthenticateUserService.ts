bc001c20c0858f69cdbe8d4ca4b40a7c17836fb6
kefflen <55823300+kefflen@users.noreply.github.com>
2021-10-22 14:15:13 -0300

    Should give to getProfileFromGithub method the code received

--- backend/src/domain/services/AuthenticateUserService.ts
+++ backend/src/domain/services/AuthenticateUserService.ts
@@ -1,7 +1,13 @@
 
 class AuthenticateUserService {
   async execute(code: string) {
+    await this.getProfileFromGithub(code)
+    
     return ''
+  }
+
+  async getProfileFromGithub(code: string) {
+
   }
 }
 
