23dd680ffc044b1c24ff7206fd951f22cf6ab0dc
kefflen <55823300+kefflen@users.noreply.github.com>
2021-10-24 19:17:21 -0300

    webSocket

--- backend/src/main/config/app.ts
+++ backend/src/main/config/app.ts
@@ -1,11 +1,21 @@
 import express from 'express'
 import setup from './setup'
 import setuptRoutes from './setuptRoutes'
+import http from 'http'
+import { Server } from 'socket.io'
 
 
 const app = express()
 
+const httpServer = http.createServer(app)
+const io = new Server(httpServer, {
+  cors: {origin: '*'}
+})
+
 setup(app)
 setuptRoutes(app)
 
-export default app
+export {
+  io
+}
+export default httpServer
