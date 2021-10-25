44ec3dce201196b9ec00da867055a0d4bf608667
kefflen <55823300+kefflen@users.noreply.github.com>
2021-10-25 13:56:22 -0300

    Registering messagesRoutes

--- backend/src/main/config/setuptRoutes.ts
+++ backend/src/main/config/setuptRoutes.ts
@@ -1,7 +1,9 @@
 import { Express } from "express"
 import { userRouter } from "../routes"
+import { messageRouter } from "../routes/MessagesRouter"
 
 
 export default (app: Express) => {
   app.use('/user', userRouter)
+  app.use('/messages', messageRouter)
 }
